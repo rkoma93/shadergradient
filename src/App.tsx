004ee0f71bd00a5550078235d4b40d8dec65be71
Marco Magni <155746143+rkoma93@users.noreply.github.com>
2026-01-21 16:31:04 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -51,8 +51,8 @@ function App() {
   return (
     <div
       style={{
-        width: '100%',
-        height: '100%',
+        //width: '100%',
+        //height: '100%',
         margin: 0,
         padding: 0,
         overflow: 'hidden',
