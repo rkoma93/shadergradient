193dee4b07a7d7a2c51574959ea6d3cf332990bf
Marco Magni <155746143+rkoma93@users.noreply.github.com>
2026-01-21 16:07:34 +0100

    Change width and height to viewport units

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ function App() {
   return (
     <div
       style={{
-        width: '100%',
-        height: '100%',
+        width: '100vw',
+        height: '100vh',
         margin: 0,
         overflow: 'hidden',
         position: 'relative',
